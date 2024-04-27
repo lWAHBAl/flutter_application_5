4f3a1337842f41055437569b78453b61df89f688
Ahmed Kabary <71717647+KaboOA@users.noreply.github.com>
2024-04-28 01:28:50 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_5/R.dart';
 import 'package:flutter_application_5/requests.dart';
 import 'package:flutter_application_5/Homelayout.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
